f272f80d685762fd8ddd7a0fb58ee7286e181d6b
yindou.666 <yindou.666@bytedance.com>
2022-09-08 17:49:25 +0800

    feat: draw two object

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -12,10 +12,18 @@ public class CameraRenderer
         name = bufferName
     };
 
+    CullingResults cullingResults;
+    static ShaderTagId unlitShaderTagId = new ShaderTagId("SRPDefaultUnlit");
+
     public void Render(ScriptableRenderContext context, Camera camera)
     {
         this.context = context;
         this.camera = camera;
+
+        if (!Cull())
+        {
+            return;
+        }
 
         Setup();
         DrawVisibleGeometry();
@@ -24,7 +32,26 @@ public class CameraRenderer
 
     void DrawVisibleGeometry()
     {
+        var sortSettings = new SortingSettings(camera)
+        {
+            criteria = SortingCriteria.CommonOpaque
+        };
+        var drawSettings = new DrawingSettings(unlitShaderTagId, sortSettings);
+        var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+
+        context.DrawRenderers(
+            cullingResults, ref drawSettings, ref filteringSettings
+        );
+
         context.DrawSkybox(camera);
+
+        sortSettings.criteria = SortingCriteria.CommonTransparent;
+        drawSettings.sortingSettings = sortSettings;
+        filteringSettings.renderQueueRange = RenderQueueRange.transparent;
+
+        context.DrawRenderers(
+            cullingResults, ref drawSettings, ref filteringSettings
+        );
     }
 
     void Setup()
@@ -47,5 +74,16 @@ public class CameraRenderer
     {
         context.ExecuteCommandBuffer(buffer);
         buffer.Clear();
+    }
+
+    bool Cull()
+    {
+        if (camera.TryGetCullingParameters(out ScriptableCullingParameters p))
+        {
+            cullingResults = context.Cull(ref p);
+            return true;
+        }
+    
+        return false;
     }
 }
